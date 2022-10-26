188131d82093c14711eb33391f6fee2e18908a73
rayzeinkah <adrien.simon@epitech.eu>
2022-10-26 10:31:33 +0200

    début du front

---
+++ gotham-front/src/components/User.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navUser">
+    <a href="/">Se connecter</a>
+  </div>
+</template>
+
+<style scoped>
+.navUser{
+    background-color: rgb(173, 173, 238);
+    position: fixed;
+    top: 0;
+    width: 100%;
+    border: 1px;
+}
+</style>
