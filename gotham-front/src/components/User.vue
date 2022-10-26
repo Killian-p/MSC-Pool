32ad80a097fbc80518d37946ea7928fd91610ffa
rayzeinkah <adrien.simon@epitech.eu>
2022-10-26 11:10:53 +0200

    ça bouge

--- gotham-front/src/components/User.vue
+++ gotham-front/src/components/User.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="navUser">
-    <a href="/">Se connecter</a>
+    <div class="nav">
+        <div class="box">
+            <a href="/">Se connecter</a>
+        </div>
+        <div class="box">
+            <a href="/workingTimes">workingTimes</a>
+        </div>
+        <div class="box">
+            <a href="/workingTime">workingTime</a>
+        </div>
+        <div class="box">
+            <a href="/clock">clock</a>
+        </div>
+        <div class="box">
+            <a href="/chartManager">chartManager</a>
+        </div>
+    </div>
+    
   </div>
 </template>
 
@@ -12,5 +29,12 @@
     width: 100%;
     border: 1px;
     height: 10vh;
+}
+.nav{
+    display: flex;
+    justify-content: space-between;
+}
+.box{
+    margin: 2px;
 }
 </style>
