80d5f5c9cc728ba9b06e1122f7c757e0c7c954ab
rayzeinkah <adrien.simon@epitech.eu>
2022-10-27 16:18:35 +0200

    worktime

--- gotham-front/src/components/WorkingTimes.vue
+++ gotham-front/src/components/WorkingTimes.vue
@@ -8,8 +8,11 @@ import axios from 'axios'
 export default {
   components: {
   },
-  props: [
-  ],
+  props: {
+    idUser: {
+      type: Number,
+    }
+  },
   data () {
     return {
       startTime: null,
@@ -23,13 +26,17 @@ export default {
       workTime: null,
     }
   },
-  mounted () {
+  async mounted () {
+    await this.seeWorkingTimes();
+    console.log(this.labelStart[15]);
+    console.log(this.workTime[15]);
+    debugger;
     new Chart(document.getElementById("line-chart"), {
       type: 'line',
       data: {
-        labels: [this.labelStart],
+        labels: this.labelStart,
         datasets: [{
-          data: [2,3,5],
+          data: this.workTime,
           label: "User1",
           borderColor: "#3e95cd",
           fill: false
@@ -42,36 +49,44 @@ export default {
         }
       }
     });
+
   },
   created () {
-    this.seeWorkingTimes();
+    
   },
   computed: {
   },
   methods: {
-    seeWorkingTimes(){
-        axios.get("http://localhost:4000/api/workingtimes/1/1")
+    async seeWorkingTimes(){
+        await axios.get(`http://localhost:4000/api/workingtimes/${this.idUser}`, {params :{
+          start: new Date("2010-07-08T06:00:00Z").toISOString(),
+        }})
         .then((response) => {
-          this.endTime = response.data.data.end
-          this.startTime = response.data.data.start
+          let arrayOfDates = response.data.data;
+          this.labelStart = new Array();
+          this.labelEnd = new Array();
+          this.workTime = new Array();
+          arrayOfDates.forEach(_ => 
+          {
+          this.arrayStart = _.start.split("T");
+          this.arrayEnd = _.end.split("T");
 
-          this.arrayStart = this.startTime.split("T")
-          this.arrayEnd = this.endTime.split("T")
+          this.startDate = parseInt(this.arrayStart[1]);
+          this.endDate = parseInt(this.arrayEnd[1]);
 
-          this.startDate = parseInt(this.arrayStart[1])
-          this.endDate = parseInt(this.arrayEnd[1])
+          this.labelStart.push(this.arrayStart[0].toString());
+          this.labelEnd.push(this.arrayEnd[0].toString());
 
-          this.labelStart = this.arrayStart[0]
-          this.labelEnd = this.arrayEnd[0]
-
-          this.workTime = this.endDate - this.startDate
+          console.log(this.endDate);
+          console.log(this.startDate);
+          this.workTime.push(this.endDate - this.startDate);
           
-            console.log(this.startDate);
-            console.log(this.endDate);
-            console.log(this.labelStart);
-            console.log(this.labelEnd);
-            console.log(this.workTime);
-            
+
+          })
+          // console.log(this.labelStart);
+          // console.log(this.labelEnd);
+          // console.log(this.workTime);
+          
         })
         .catch(console.error);
     },
