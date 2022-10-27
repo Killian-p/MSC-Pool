966f8dcdfa9173c71a8133146c23aeba88003831
victorMusta <victorgrabowski33@gmail.com>
2022-10-27 09:55:59 +0200

    avancement workingtimes

--- gotham-front/src/components/WorkingTime.vue
+++ gotham-front/src/components/WorkingTime.vue
@@ -1,7 +1,47 @@
 <template>
-  
+    <form v-on:submit.prevent="createWorkingTime" method="post">
+        <label>
+            starting date :
+        </label>
+        <input type="date" id="startingDate" v-model="startingDate" required/>
+        <label>
+            ending date :
+        </label>
+        <input type="date" id="endingDate" v-model="endingDate" required/>
+        <button type="submit">
+        Créer un nouveau working Time
+        </button>
+        </form>
 </template>
 
-<style scoped>
+<script>
+import axios from 'axios'
+export default {
+  components: {
+  },
+  props: [
+  ],
+  data () {
+    return {
+        startingDate: '',
+        endingDate: '',
+    }
+  },
+  mounted () {
+  },
+  created () {
+  },
+  computed: {
+  },
+  methods: {
+    createWorkingTime(){
+      axios.post("http://localhost:4000/api/workingtimes")
+    }
+  },
+  watch: {
+  }
+}
+</script>
 
+<style scoped lang="scss">
 </style>
