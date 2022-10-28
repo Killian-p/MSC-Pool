3b291f2743b721c2100d9d4743c540fee468021a
victorMusta <victorgrabowski33@gmail.com>
2022-10-28 16:19:03 +0200

    clock manager avancé (ne marche pas)

--- gotham-front/src/components/ClockManager.vue
+++ gotham-front/src/components/ClockManager.vue
@@ -1,16 +1,41 @@
 <template>
-  bbb
+  <button @click="userClocks">
+        Créer un nouveau working Time
+        </button>
 </template>
 
 <script>
+// import axios from 'axios'
 export default {
+  components: {
+  },
   props: {
     idUser: {
       type: Number,
     }
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+  },
+  created () {
+  },
+  computed: {
+  },
+  methods: {
+    async userClocks(){
+      console.log(new Date(Date.now()))
+        // await axios.get(`http://localhost:4000/api/workingtimes/${this.idUser}`, {params :{
+        //   start: new Date().toISOString(),
+        // }})
+    },
+  },
+  watch: {
   }
 }
 </script>
-<style scoped>
 
+<style scoped lang="scss">
 </style>
