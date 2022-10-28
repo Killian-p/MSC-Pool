52345aeb552909bb80af63f69124df70e9f70902
killian <killian.planchot@epitech.eu>
2022-10-28 16:16:34 +0200

    adding docker compose for front and back

--- gotham/config/dev.exs
+++ gotham/config/dev.exs
@@ -2,10 +2,7 @@ import Config
 
 # Configure your database
 config :gotham, Gotham.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "gotham_dev",
+  url: "ecto://postgres:postgres@localhost:5432/gotham_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +16,7 @@ config :gotham, Gotham.Repo,
 config :gotham, GothamWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
--- gotham/lib/gotham/repo.ex
+++ gotham/lib/gotham/repo.ex
@@ -2,4 +2,13 @@ defmodule Gotham.Repo do
   use Ecto.Repo,
     otp_app: :gotham,
     adapter: Ecto.Adapters.Postgres
+  
+  def init(_type, config) do
+    db_url = System.get_env("DATABASE_URL")
+    if !is_nil(db_url) do
+      {:ok, Keyword.put(config, :url, db_url)}
+    else
+      {:ok, config}
+    end
+  end
 end
