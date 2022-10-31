a1d9927afd9faabe094cb075ad4d471cd0091f99
rayzeinkah <adrien.simon@epitech.eu>
2022-10-31 11:28:03 +0100

    tableau working times

--- gotham-front/src/components/WorkingTimes.vue
+++ gotham-front/src/components/WorkingTimes.vue
@@ -1,8 +1,22 @@
 <template>
-  workingtimes
+  <table>
+    <thead>
+      <th>Id</th>
+      <th>Start</th>
+      <th>End</th>
+    </thead>
+    <tbody>
+      <tr v-for="worktime in datas">
+        <td>{{worktime.id}}</td>
+        <td>{{worktime.start}}</td>
+        <td>{{worktime.end}}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   props: {
     idUser: {
@@ -11,11 +25,24 @@ export default {
   },
   data () {
     return {
+      datas: null,
     }
   },
   mounted () {
-   
-
+    axios.get(`http://localhost:4000/api/workingtimes/${this.idUser}`, {params :{
+          start: new Date("1900-07-08T06:00:00Z").toISOString(),
+        }}).then(_ => {
+          this.datas = _.data.data;
+          let start;
+          let end;
+          this.datas.forEach(x => {
+            start = x.start.split("T");
+            end = x.end.split("T");
+            x.start = start[0] + " " + start[1];
+            x.end = end[0] + " " + end[1];
+          })
+          debugger;
+        });
   },
   created () {
     
