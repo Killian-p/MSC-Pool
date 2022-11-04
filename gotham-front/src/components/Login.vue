cec808a3e1c8a426b82460f1e4cf6b8a04224dd6
rayzeinkah <adrien.simon@epitech.eu>
2022-11-04 14:55:53 +0100

    changement url

--- gotham-front/src/components/Login.vue
+++ gotham-front/src/components/Login.vue
@@ -79,7 +79,7 @@ export default {
   },
   methods: {
     createUser(){
-        axios.post('${import.meta.env.VITE_BACKEND_URL}/api/users/sign_up', {
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/sign_up`, {
             user:{
                 username: this.username,
                 email: this.email,
@@ -98,7 +98,7 @@ export default {
         this.connected=true;
     },
     getUser(){
-        axios.post('${import.meta.env.VITE_BACKEND_URL}/api/users/sign_in', {
+        axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/users/sign_in`, {
             password: this.password,
             email: this.email,
          })
